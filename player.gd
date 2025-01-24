796f7a2b78db494c8fff9a22768b6800e91776de
L4mbads <rizacal.mamen@gmail.com>
2025-01-24 21:38:50 +0700

    refactor: separate functionality

--- player.gd
+++ player.gd
@@ -1,21 +1,13 @@
-extends Sprite2D
+extends Node2D
 class_name Player
 
 @onready var tile_map_layer: TileMapLayer = $"../TileMapLayer"
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sprite_2d: Sprite2D = $Sprite
+@onready var target: Sprite2D = $Target
 
 var is_moving = false
 
-func _physics_process(delta: float) -> void:
-	if not is_moving:
-		return
 	
-	if global_position == sprite_2d.global_position:
-		is_moving = false
-		return
-	
-	sprite_2d.global_position = sprite_2d.global_position.move_toward(global_position, 1)
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if is_moving:
@@ -29,8 +21,11 @@ func _process(delta: float) -> void:
 	elif Input.is_action_pressed("right"):
 		_move(Vector2.RIGHT)
 
+func _set_is_moving(val):
+	is_moving = val
 
-func _move(direction):
+
+func get_next_tile(direction) -> Vector2i:
 	var curr_tile = tile_map_layer.local_to_map(global_position)
 	
 	var next_tile = Vector2i(curr_tile.x + direction.x, curr_tile.y + direction.y)
@@ -38,8 +33,25 @@ func _move(direction):
 	var tile_data = tile_map_layer.get_cell_tile_data(next_tile)
 	
 	if tile_data == null or tile_data.get_custom_data("walkable") == false:
-		return
+		return Vector2i(999,999)
 	
-	is_moving = true
+	return next_tile
+
+func move(next_tile):
 	global_position = tile_map_layer.map_to_local(next_tile)
+
+
+func _move(direction):
+
+	var next_tile = get_next_tile(direction)
+	if next_tile == Vector2i(999, 999): return
+	is_moving = true
+	
+	var curr_tile = tile_map_layer.local_to_map(global_position)
+	move(next_tile)
 	sprite_2d.global_position = tile_map_layer.map_to_local(curr_tile)
+	
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE)
+	tween.tween_property(sprite_2d, "global_position", tile_map_layer.map_to_local(next_tile), 1)
+	tween.tween_callback(_set_is_moving.bind(false))
+	#sprite_2d.global_position = tile_map_layer.map_to_local(curr_tile)
---
+++ stage_manager.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var move_count = 0
+var time_elapsed = 0
+var is_stopped = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if !is_stopped:
+		time_elapsed += delta
+		#$Label.text = str(time_elapsed).pad_decimals(2)
