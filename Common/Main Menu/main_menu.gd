48171d7921bc61a3210a44d50a48ba8ab5227d6a
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-01-25 11:48:26 +0700

    feat: main menu ui

---
+++ Common/Main Menu/main_menu.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_close_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_play_button_pressed() -> void:
+	pass 
+
+# flow
+# case: belum pernah main
+# play button -> cutscene lore -> tutorial (stage 1) -> stage selection -> pilih stage atau exit
+# case: udah pernah main
+# play button -> stage selection -> bla bla bla
+
+func _on_setting_button_pressed() -> void:
+	pass # Replace with function body.
